9c2c4c2e6b2739a82b4375aee09519812e1a8629
yurii19 <yurikolechkin@gmail.com>
2024-05-01 10:28:45 +0300

    chore: edit userInfo form develop

--- src/app/pages/edit-profile/about-form/about-form.component.ts
+++ src/app/pages/edit-profile/about-form/about-form.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-about-form',
   templateUrl: './about-form.component.html',
-  styleUrls: ['./about-form.component.css']
+  styleUrls: ['./about-form.component.css'],
 })
 export class AboutFormComponent {
+  
+  languages: string[] = ['Ukrainian', 'English'];
+  languagesSet = ['Ukrainian', 'English', 'Frech', 'Spain', 'Poland'];
+  form = new FormGroup({
+    availability: new FormControl(''),
+    occupation: new FormControl(''),
+    education: new FormControl(''),
+    languages: new FormControl(''),
+    aboutText: new FormControl(''),
+  });
 
+  removeLanguage(language: string) {
+    this.languages = [...this.languages.filter((l) => l !== language)];
+    this.form.get('languages')?.setValue('');
+  }
+
+  addLanguage() {
+    const newLanguage = this.form.get('languages')?.value;
+    if (newLanguage && this.languages.includes(newLanguage)) {
+      return;
+    } else {
+      this.languages.push(newLanguage as string);
+    }
+  }
 }

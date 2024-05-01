291d96a1affeaa2bc4a3786f753e1bc42888043c
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2024-05-01 10:23:33 +0300

    Implamented User public trips MVP

--- src/app/pages/public-trips/public-trips.component.ts
+++ src/app/pages/public-trips/public-trips.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Request} from 'src/app/services/models/request';
 import {FormsService} from "../../services/forms/forms.service";
 import {RequestsService} from "../../services/services/requests.service";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-public-trips',
@@ -15,11 +16,23 @@ export class PublicTripsComponent implements OnInit {
   trips: Request[] = [];
 
   constructor(
+    private router: Router,
     private formsService: FormsService,
     private requestsService: RequestsService
   ) {}
 
   ngOnInit(): void {
+    this.loadData();
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.urlAfterRedirects === '/dashboard/public-trips') {
+          this.loadData();
+        }
+      }
+    });
+  }
+
+  loadData() {
     this.requestsService.getOutgoingRequests({
       page: this.page,
       size: this.size
@@ -31,9 +44,8 @@ export class PublicTripsComponent implements OnInit {
       error: (err) => {
         console.log(err);
       }
-    })
+    });
   }
-
 
   edit(trip: Request) {
     this.formsService.setRequest(trip)
--- src/app/services/forms/forms.service.ts
+++ src/app/services/forms/forms.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
-import { Request } from "../models/request";
-import { FormControl, FormGroup } from '@angular/forms';
+import {Request} from "../models/request";
+import {FormControl, FormGroup} from '@angular/forms';
+import {UsersService} from "../services/users.service";
+import {User} from "../models/user";
+import * as moment from "moment";
 
 @Injectable({
   providedIn: 'root',
@@ -8,33 +11,48 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class FormsService {
   currentRequest: Request = {} as Request;
 
-  constructor() {}
+  constructor(
+    private usersService: UsersService
+  ) {}
 
   setRequest(r: Request) {
     this.currentRequest = r;
   }
 
   initRequestForm(req: Request) {
-    return new FormGroup({
+    const formGroup = new FormGroup({
       destination: new FormControl(req.location),
-      arrival: new FormControl(req.from),
-      departure: new FormControl(req.to),
+      arrival: new FormControl(moment(req.from).format('YYYY-MM-DD')),
+      departure: new FormControl(moment(req.to).format('YYYY-MM-DD')),
       travelers: new FormControl(req.travelersAmount),
       description: new FormControl(req.message),
-      host: new FormControl(req.receiver),
+      host: new FormControl(''),
     });
+
+    this.usersService.getUserById({userId: req.receiver as string}).subscribe({
+      next: (res) => {
+        const host = res as User;
+        console.log('Host was found: ' + host.id);
+
+        formGroup.get('host')?.setValue(host.fullName as string);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+
+    return formGroup;
   }
 
-  createRequestFromFormValues(formValues: any, id: string): Request{
+  createRequestFromFormValues(formValues: any, senderId: string, receiverId: string): Request {
     return {
-      id: 'string',
-      sender: id,
-      receiver: 'string',
+      sender: senderId,
+      receiver: receiverId,
       travelersAmount: formValues.travelers,
-      from: formValues.departure,
-      to: formValues.arrival,
+      from: moment(formValues.arrival, 'YYYY-MM-DD').valueOf(),
+      to: moment(formValues.departure, 'YYYY-MM-DD').valueOf(),
       message: formValues.description,
-      serviceType: 'ACCOMMODATION_PROVISION',
+      serviceType: 'ACCOMMODATION_REQUEST',
       location: formValues.destination,
       requestStatus: 'CREATED',
     };
